e4c1b96b21485732784a6c15d600e851c7112c45
Lorenzo Manacorda <lorenzo@mailbox.org>
2023-03-27 09:59:29 +0200

    Revert "wip"
    This reverts commit 82c37861174a9c9f138501e7774c20b60c2be0ae.

--- doc/doc-support/lib-function-docs.nix
+++ doc/doc-support/lib-function-docs.nix
@@ -11,17 +11,25 @@ with pkgs; stdenv.mkDerivation {
     function docgen {
       # TODO: wrap lib.$1 in <literal>, make nixdoc not escape it
       if [[ -e "../lib/$1.nix" ]]; then
-        nixdoc --category "$1" --description "lib.$1: $2" --file "$1.nix" >> "$out/index.md"
+        nixdoc -c "$1" -d "lib.$1: $2" -f "$1.nix" > "$out/$1.xml"
       else
-        nixdoc --category "$1" --description "lib.$1: $2" --file "$1/default.nix" >> "$out/index.md"
+        nixdoc -c "$1" -d "lib.$1: $2" -f "$1/default.nix" > "$out/$1.xml"
       fi
+      echo "<xi:include href='$1.xml' />" >> "$out/index.xml"
     }
 
     mkdir -p "$out"
 
+    cat > "$out/index.xml" << 'EOF'
+    <?xml version="1.0" encoding="utf-8"?>
+    <root xmlns:xi="http://www.w3.org/2001/XInclude">
+    EOF
+
     ${lib.concatMapStrings ({ name, description }: ''
       docgen ${name} ${lib.escapeShellArg description}
     '') libsets}
+
+    echo "</root>" >> "$out/index.xml"
 
     ln -s ${locationsXml} $out/locations.xml
   '';
